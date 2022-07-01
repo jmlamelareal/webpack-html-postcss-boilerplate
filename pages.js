d2cc2e8811ddd5c35eb07214303dd283cefe04b4
José María Lamela <jmlamelareal@gmail.com>
2022-07-01 20:36:45 +0200

    refactor and add css and js support

---
+++ pages.js
@@ -0,0 +1,8 @@
+module.exports = [
+    {
+        filename: 'index.html',
+    },
+    {
+        filename: 'other-page.html',
+    },
+];
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,60 @@
+const Pages = require('./pages.js');
+
+console.log(Pages);
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 const path = require('path');
 
 module.exports = {
     mode: 'development',
     target: 'web',
-    plugins: [
-        new HtmlWebpackPlugin({
-            hash: true,
-            title: 'Webpack Example App',
-            header: 'Webpack Example Title',
-            metaDesc: 'Webpack Example Description',
-            template: './src/index.html',
-            filename: 'index.html',
-            inject: 'body'
-        })
-    ],
+    entry: {
+        index: './src/main.js',
+    },
     output: {
         clean: true
     },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader"
+                ]
+            }
+        ]
+    },
+    plugins: [].concat(
+        Pages.map(
+            (page) =>
+                new HtmlWebpackPlugin({
+                    hash: true,
+                    inject: true,
+                    title: 'Webpack Example App',
+                    header: 'Webpack Example Title',
+                    metaDesc: 'Webpack Example Description',
+                    template: `./src/${page.filename}`,
+                    filename: `${page.filename}`,
+                    inject: 'body',
+                })
+        )
+    ).concat(
+        new MiniCssExtractPlugin({
+            filename: "[name].[contenthash].css",
+            chunkFilename: "[id].[contenthash].css"
+        })
+    ),
     devServer: {
         hot: false,
         static: {
